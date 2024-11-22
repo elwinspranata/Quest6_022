2411955d407dae6cfe3da823ff978deea422e506
elwinspranata <elwinpranata02@gmail.com>
2024-11-22 16:27:07 +0700

    Tambah data class Mahasiswa dengan properti nim, nama, dan email

--- app/src/main/java/com/example/navigasidengandata/model/Mahasiswa.kt
+++ app/src/main/java/com/example/navigasidengandata/model/Mahasiswa.kt
@@ -2,7 +2,7 @@ package com.example.navigasidengandata.model
 
 import android.provider.ContactsContract.CommonDataKinds.Email
 
-data class Mahasiswa(
+data class Mahasiswa (
     val nim: String ="",
     val nama: String = "",
     val email: String = "",
